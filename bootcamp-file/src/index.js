75e5c3222e3ed8ae3b129ae234743f7f23068d70
mirabor <mirawithyu@gmail.com>
2023-12-08 12:37:01 -0500

    Firebase initialization

--- bootcamp-file/src/index.js
+++ bootcamp-file/src/index.js
@@ -3,7 +3,26 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnJOKtEmy-2P9cYVTCSXBBpSzPN3iJtGk",
+  authDomain: "bootcamp-6d8fe.firebaseapp.com",
+  projectId: "bootcamp-6d8fe",
+  storageBucket: "bootcamp-6d8fe.appspot.com",
+  messagingSenderId: "234774164778",
+  appId: "1:234774164778:web:95639a3005543fc6b11a53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
 );
+
