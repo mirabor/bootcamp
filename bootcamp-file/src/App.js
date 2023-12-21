7014a7bd578ca683aca40e65314024fabb70c981
mirabor <mirawithyu@gmail.com>
2023-12-20 23:23:08 -0500

    Part 3 Task #1 Homepage

--- bootcamp-file/src/App.js
+++ bootcamp-file/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CardEditor from './CardEditor';
 import CardViewer from './CardViewer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './Homepage';
 
 // I am so very confused
 
@@ -52,6 +53,7 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <Routes>
+          {/* <Route path="/" element={<Homepage />} /> */}
           <Route path="/editor" element={<CardEditor addCard={this.addCard} cards={this.state.cards} deleteCard={this.deleteCard} />} />
           <Route path="/viewer" element={<CardViewer cards={this.state.cards} />} />
         </Routes>
