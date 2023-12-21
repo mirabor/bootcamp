a7a1579ac113f238223e71debbe4a38fcbb588b1
mirabor <mirawithyu@gmail.com>
2023-12-21 00:01:44 -0500

    Part 3 connecting firebase/react with react-redux-firebase package

--- bootcamp-file/src/index.js
+++ bootcamp-file/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
+import { createStore, combineReducers } from 'redux';
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer,
+} from 'react-redux-firebase';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCnJOKtEmy-2P9cYVTCSXBBpSzPN3iJtGk",
   authDomain: "bootcamp-6d8fe.firebaseapp.com",
@@ -20,9 +28,34 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const firebase = app;
+
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+});
+
+// Create store with reducers and initial state
+const store = createStore(rootReducer, composeWithDevTools());
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+};
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+};
 
 ReactDOM.render(
-  <App/>,
+<Provider store={store}>
+  <ReactReduxFirebaseProvider {...rrfProps}>
+    <Router>
+      <App />
+    </Router>
+  </ReactReduxFirebaseProvider>
+</Provider>,
   document.getElementById('root')
 );
 
