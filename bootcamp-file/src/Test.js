b4ed13a8d85b804fd67cf70cf912d196d6f69928
mirabor <mirawithyu@gmail.com>
2023-12-24 16:43:41 -0500

    Connect mapStateToProps

---
+++ bootcamp-file/src/Test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Test = props => {
+  return <div>Test</div>;
+}
+
+const mapStateToProps = state => {
+  console.log(state);
+  const isEmpty = state.firebase.profile.isEmpty;
+  return { test: '1234', hello: 'world', isEmpty: isEmpty };
+};
+
+export default connect(mapStateToProps)(Test);
