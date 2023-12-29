c50e9724c8d0f2b4b7ba3ffab20a2afeaa672bd9
mirabor <mirawithyu@gmail.com>
2023-12-28 21:00:54 -0500

    Part 4 CardEditor component

--- bootcamp-file/src/App.js
+++ bootcamp-file/src/App.js
@@ -4,68 +4,23 @@ import CardViewer from './CardViewer';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './Homepage';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cards: [
-        { front: 'front1', back: 'back1' },
-        { front: 'front2', back: 'back2' },
-      ],
-      cardIndex: 0,
-    };
-  }
-
-  addCard = card => {
-    const cards = this.state.cards.slice().concat(card);
-    this.setState({ cards });
-  };
-
-  deleteCard = index => {
-    const cards = this.state.cards.slice();
-    cards.splice(index, 1);
-
-    if (cards.length === 0) {
-      cards.push({ front: 'Default Front', back: 'Default Back' });
-    }
-
-    this.setState(prevState => ({
-      cards,
-      cardIndex: Math.min(prevState.cardIndex, cards.length - 1),
-    }));
-  };
-
-  goToNextCard = () => {
-    this.setState(prevState => ({
-      cardIndex: Math.min(prevState.cardIndex + 1, prevState.cards.length - 1),
-    }));
-  };
-
-  goToPrevCard = () => {
-    this.setState(prevState => ({
-      cardIndex: Math.max(prevState.cardIndex - 1, 0),
-    }));
-  };
-
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/">
-          <Homepage />
-        </Route>
-        <Route exact path="/editor">
-          <CardEditor
-            addCard={this.addCard}
-            cards={this.state.cards}
-            deleteCard={this.deleteCard}
-          />
-        </Route>
-        <Route exact path="/viewer/:deckId">
-          <CardViewer cards={this.state.cards} />
-        </Route>
-      </Switch>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Homepage />
+      </Route>
+      <Route exact path="/editor">
+        <CardEditor />
+      </Route>
+      <Route exact path="/viewer/:deckId">
+        <CardViewer />
+      </Route>
+      <Route>
+        <div>Page not found!</div>
+      </Route>
+    </Switch>
+  );
+};
 
 export default App;
